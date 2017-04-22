4484b21bde243278b5fa8a153e9b96d45da95315
Korey Huskonen <huskonkw@mail.uc.edu>
2017-04-22 18:49:27 -0400

    Complete inheritance

--- Monopoly.h
+++ Monopoly.h
@@ -4,14 +4,14 @@
 
 using namespace std;
 
+class Location;
 class Property;
 class Eatery;
 class Street;
 
 class Player {
     string name;
-    double money;
-    int position;
+    int position, money;
     bool jailed;
     vector<Property> properties;
     vector<Eatery> eateries;
@@ -19,78 +19,76 @@ class Player {
 public:
     Player(string player_name){
         name = player_name;
-        money = 1500;
         position = 0;
+        money = 1500;
         jailed = false;
     }
     string getName(){return name;}
-    double getMoney(){return money;}
+    int getPos(){return position;}
+    int getMoney(){return money;}
     void addMoney(int amount){money += amount;}
     void subMoney(int amount){money -= amount;}
-    int getPos(){return position;}
-    void setPos(int pos){position = pos;}
-    void move(int amount){position = (position + amount) % 40;}
     bool in_jail(){return jailed;}
+    void move(int amount){position = (position + amount) % 40;}
     vector<Property> getProperties(){return properties;}
     vector<Eatery> getEateries(){return eateries;}
     vector<Street> getStreets(){return streets;}
     bool canAfford(int price){return money - price > 0;}
 };
 
-class Property {
-    int position, price, houses, house_price, mortgage, rent0, rent1, rent2, rent3, rent4, rent5;
-    string name, color;
+class Location {
+protected:
+    int position, price, mortgage;
+    string name;
     bool owned;
     Player *owner;
 public:
-    Property(string p_name, string p_color, int pos, int p_price, int mort, int h_price, int r0, int r1, int r2, int r3, int r4, int r5){
-        name = p_name;
-        color = p_color;
-        position = pos;
-        price = p_price;
-        mortgage = mort;
-        house_price = h_price;
-        rent0 = r0; rent1 = r1; rent2 = r2; rent3 = r3; rent4 = r4; rent5 = r5;
-        houses = 0; owned = false; owner = NULL;
+    Location(string lname, int lpos, int lprice, int lmort)
+        : name(lname), position(lpos), price(lprice), mortgage(lmort)
+    {
+        owned = false; owner = NULL;
     }
+
     int getPos(){return position;}
     int getPrice(){return price;}
-    int getHouses(){return houses;}
-    int getHousePrice(){return house_price;}
     int getMortgage(){return mortgage;}
     string getName(){return name;}
-    string getColor(){return color;}
     bool is_owned(){return owned;}
     string getOwner(){return owner->getName();}
-    int getRent(){
-        switch(houses){
-            case 0:
-                return rent0;
-            case 1:
-                return rent1;
-            case 2:
-                return rent2;
-            case 3:
-                return rent3;
-            case 4:
-                return rent4;
-            case 5:
-                return rent5;
-            default:
-                return rent0;
-        }
-    }
-    void pay_rent(Player &rent_payer){
+    virtual int getRent(){return 0;}
+    virtual void pay_rent(Player &rent_payer){
         int rent = getRent();
         rent_payer.subMoney(rent);
         owner->addMoney(rent);
     }
     void change_owner(Player *new_owner){owner = new_owner;}
-    bool buy_property(Player *player){
+    bool buy_location(Player *player){
         if(!player->canAfford(price)) return false;
         player->subMoney(price);
         change_owner(player);
         return true;
+    }
+};
+
+class Property : public Location {
+    int houses, house_price, rent0, rent1, rent2, rent3, rent4, rent5;
+    string color;
+public:
+    Property(string lname, string lcolor, int lpos, int lprice, int lmort, int hprice, int r0, int r1, int r2, int r3, int r4, int r5)
+        : Location(lname, lpos, lprice, lmort), color(lcolor), house_price(hprice),
+          rent0(r0), rent1(r1), rent2(r2), rent3(r3), rent4(r4), rent5(r5)
+    {
+        houses = 0;
+    }
+
+    string getColor(){return color;}
+    int getRent(){
+        if(houses == 0) return rent0;
+        if(houses == 1) return rent1;
+        if(houses == 2) return rent2;
+        if(houses == 3) return rent3;
+        if(houses == 4) return rent4;
+        return rent5;
     }
     int colorGroupSize(){
         if (color == "brown" || color == "dark blue") return 2;
@@ -111,25 +109,11 @@ public:
     }
 };
 
-class Eatery {
-    int position, price, mortgage;
-    string name;
-    bool owned;
-    Player *owner;
+class Eatery : public Location {
 public:
-    Eatery(string e_name, int e_pos, int e_price, int mort){
-        name = e_name;
-        position = e_pos;
-        price = e_price;
-        mortgage = mort;
-        owned = false; owner = NULL;
-    }
-    int getPos(){return position;}
-    int getPrice(){return price;}
-    int getMortgage(){return mortgage;}
-    string getName(){return name;}
-    bool is_owned(){return owned;}
-    string getOwner(){return owner->getName();}
+    Eatery(string lname, int lpos, int lprice, int lmort)
+        : Location(lname, lpos, lprice, lmort) {}
+
     int getRent(int dice_roll){
         int count = 0;
         while(count < owner->getEateries().size()) count++;
@@ -141,49 +125,16 @@ public:
         rent_payer.subMoney(rent);
         owner->addMoney(rent);
     }
-    void change_owner(Player *new_owner){owner = new_owner;}
-    bool buy_eatery(Player *player){
-        if(!player->canAfford(price)) return false;
-        player->subMoney(price);
-        change_owner(player);
-        return true;
-    }
 };
 
-class Street {
-    int position, price, mortgage;
-    string name;
-    bool owned;
-    Player *owner;
+class Street : public Location {
 public:
-    Street(string e_name, int e_pos, int e_price, int mort){
-        name = e_name;
-        position = e_pos;
-        price = e_price;
-        mortgage = mort;
-        owned = false; owner = NULL;
-    }
-    int getPos(){return position;}
-    int getPrice(){return price;}
-    int getMortgage(){return mortgage;}
-    string getName(){return name;}
-    bool is_owned(){return owned;}
-    string getOwner(){return owner->getName();}
+    Street(string lname, int lpos, int lprice, int lmort)
+        : Location(lname, lpos, lprice, lmort) {}
+
     int getRent(){
         int count = 0;
         while(count < owner->getStreets().size()) count++;
         return 25 * pow(2,count-1);
-    }
-    void pay_rent(Player &rent_payer){
-        int rent = getRent();
-        rent_payer.subMoney(rent);
-        owner->addMoney(rent);
-    }
-    void change_owner(Player *new_owner){owner = new_owner;}
-    bool buy_street(Player *player){
-        if(!player->canAfford(price)) return false;
-        player->subMoney(price);
-        change_owner(player);
-        return true;
     }
 };
